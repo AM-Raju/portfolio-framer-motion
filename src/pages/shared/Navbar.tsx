8c36833cd9089081732aef81b4c5f3a205a567f8
AM-Raju <mdmorshed.sj2005@gmail.com>
2024-01-24 23:48:05 +0600

    M6 Con1 Seg1 Part1 - Navbar part done

---
+++ src/pages/shared/Navbar.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  const navAnimation = {
+    hidden: {
+      y: -60,
+    },
+    visible: {
+      y: -0,
+      transition: {
+        delay: 0.3,
+        type: "spring",
+        stiffness: 120,
+      },
+    },
+  };
+
+  const navListAnimation = {
+    hover: {
+      scale: 1.1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        stiffness: 120,
+      },
+    },
+  };
+  return (
+    <motion.nav variants={navAnimation} initial="hidden" animate="visible">
+      <div className="flex justify-center items-center gap-5">
+        <motion.a variants={navListAnimation} whileHover="hover" href="">
+          My Work
+        </motion.a>
+        <motion.a variants={navListAnimation} whileHover="hover" href="">
+          About Me
+        </motion.a>
+        <motion.a variants={navListAnimation} whileHover="hover" href="">
+          Resume
+        </motion.a>
+      </div>
+    </motion.nav>
+  );
+};
+
+export default Navbar;
